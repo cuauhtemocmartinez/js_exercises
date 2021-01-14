655eca7ad950639ec3ca39bde8a3aef46ae35783
cuauhtemocmartinez <mr.cuauhtemocmartinez@gmail.com>
2021-01-14 14:10:35 -0800

    cleanup

--- modern_js/app.js
+++ modern_js/app.js
@@ -1,18 +1,12 @@
-const life = 110;
+let life = 100;
 
 life = life - 50;
-
 
 console.log(life);
 
 // var life = 100 ; // Life bar - int
-
 // life = life - 10; // 90
-
 // life = life / 10; 
-
 // var name = 'Ninja'; // Name - String
-
 // var checkout = false
-
 // var box;
