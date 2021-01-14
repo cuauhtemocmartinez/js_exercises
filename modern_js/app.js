909f38702e38a2472f40075ef80545ae4c2a4944
cuauhtemocmartinez <mr.cuauhtemocmartinez@gmail.com>
2021-01-14 14:07:21 -0800

    added const comment out var

--- modern_js/app.js
+++ modern_js/app.js
@@ -1,16 +1,18 @@
-var life = 100 ; // Life bar - int
+const life = 110;
 
-life = life - 10; // 90
-
-life = life / 10; 
-
-var name = 'Ninja'; // Name - String
-
-var checkout = false
-
-var box;
-
+life = life - 50;
 
 
 console.log(life);
 
+// var life = 100 ; // Life bar - int
+
+// life = life - 10; // 90
+
+// life = life / 10; 
+
+// var name = 'Ninja'; // Name - String
+
+// var checkout = false
+
+// var box;
