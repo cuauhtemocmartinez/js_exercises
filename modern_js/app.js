8bf2bc600300010c874d858962f21d4a67066d55
cuauhtemocmartinez <mr.cuauhtemocmartinez@gmail.com>
2021-01-17 13:59:46 -0800

    added typeof

--- modern_js/app.js
+++ modern_js/app.js
@@ -4,3 +4,6 @@ const age = 30;
 console.log("Hello, my name is " + myName + " and my age is " + age + ".");
 
 console.log(`Hello, my name is ${myName} and my age is ${age}.`);
+
+console.log(typeof myName);
+console.log(typeof age);
