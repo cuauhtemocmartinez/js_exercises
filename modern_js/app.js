9aa6d65040354d15fc7144d17d30f9d56a4631dc
cuauhtemocmartinez <mr.cuauhtemocmartinez@gmail.com>
2021-01-19 11:28:19 -0800

    added else if statement

--- modern_js/app.js
+++ modern_js/app.js
@@ -2,6 +2,10 @@ const age = 10;
 
 if(age > 18){
     console.log("You are over 18 years of age")
-} else {
+} else if(age < 15){
+    console.log("You are under 18 years of age")
+}
+
+else {
     console.log("You are under 18 years of age")
 }
