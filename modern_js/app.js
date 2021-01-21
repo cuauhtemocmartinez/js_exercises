b76950d58d448465392730d9d37db658c59215b0
cuauhtemocmartinez <mr.cuauhtemocmartinez@gmail.com>
2021-01-20 17:59:38 -0800

    access array positions

--- modern_js/app.js
+++ modern_js/app.js
@@ -2,5 +2,5 @@ const schedule = ['Wake up', 'Eat', 'Watch TV'];
 
 const phoneNumbers = [6508885555, 4158885555, 2138885555];
 
-console.log(schedule);
-console.log(phoneNumbers);
+console.log(schedule[1]);
+console.log(phoneNumbers[2]);
