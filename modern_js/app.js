8c4e46a9b22d8b6bf2ae751bfc89f79722b940e8
cuauhtemocmartinez <mr.cuauhtemocmartinez@gmail.com>
2021-01-20 17:54:17 -0800

    added schedule array

--- modern_js/app.js
+++ modern_js/app.js
@@ -1,8 +1,2 @@
-const dice1 = 6;
-const dice2 = 3;
+const schedule = ['Wake up', 'Eat', 'Watch TV'];
 
-if(dice1 === 6 || dice2 === 6){
-    console.log("You rolled a double")
-} else {
-    console.log("Nope!")
-}
