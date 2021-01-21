df03a0be3b96df7f667cc300bc466cf4b21617c6
cuauhtemocmartinez <mr.cuauhtemocmartinez@gmail.com>
2021-01-20 18:09:07 -0800

    added indexOf

--- modern_js/app.js
+++ modern_js/app.js
@@ -4,8 +4,8 @@ const phoneNumbers = [6508885555, 4158885555, 2138885555];
 
 // schedule.push('Sleep');
 // schedule.pop();
-schedule.unshift('Alarm');
+// schedule.unshift('Alarm');
 
-console.log(schedule);
-console.log(phoneNumbers);
+console.log(schedule.indexOf('Watch TV'));
+console.log(phoneNumbers.indexOf(4158885555));
 
