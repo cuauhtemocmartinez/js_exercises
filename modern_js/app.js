aab67d9fbf679aba838ec9ce1f7aaeb858e61741
cuauhtemocmartinez <mr.cuauhtemocmartinez@gmail.com>
2021-01-20 18:06:48 -0800

    added unshift

--- modern_js/app.js
+++ modern_js/app.js
@@ -4,7 +4,7 @@ const phoneNumbers = [6508885555, 4158885555, 2138885555];
 
 // schedule.push('Sleep');
 // schedule.pop();
-schedule.shift();
+schedule.unshift('Alarm');
 
 console.log(schedule);
 console.log(phoneNumbers);
