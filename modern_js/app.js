5a7e35e09ae5f550c632ce1c284b4149c30f9c2d
cuauhtemocmartinez <mr.cuauhtemocmartinez@gmail.com>
2021-01-21 16:18:35 -0800

    created objects

--- modern_js/app.js
+++ modern_js/app.js
@@ -1,11 +1,7 @@
-const schedule = ['Wake up', 'Eat', 'Watch TV'];
+const user = {
+    UserName: 'Alex',
+    age: 35,
+    purchases: ['phone', 'car', 'laptop']
+};
 
-const phoneNumbers = [6508885555, 4158885555, 2138885555];
-
-// schedule.push('Sleep');
-// schedule.pop();
-// schedule.unshift('Alarm');
-
-console.log(schedule.indexOf('Watch TV'));
-console.log(phoneNumbers.indexOf(4158885555));
-
+console.log(user.purchases)
