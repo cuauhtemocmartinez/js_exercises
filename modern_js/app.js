deb66d9f29112a8d7b28a17ede923ac20cb93c9e
cuauhtemocmartinez <mr.cuauhtemocmartinez@gmail.com>
2021-01-23 15:53:36 -0800

    added addListBtn

--- modern_js/app.js
+++ modern_js/app.js
@@ -1,7 +1,10 @@
 const userList = document.querySelectorAll(".name-list li");
+const listInput = document.querySelector('.list-input');
 
 for (user of userList) {
     user.addEventListener("click", function() {
         this.style.color = "red";
     });
 }
+
+console.log(listInput.value);
