399931572d2a23f50571eb545f444f96a8e8fb0e
cuauhtemocmartinez <mr.cuauhtemocmartinez@gmail.com>
2021-01-23 15:46:39 -0800

    added this.sytle to red

--- modern_js/app.js
+++ modern_js/app.js
@@ -2,6 +2,6 @@ const userList = document.querySelectorAll(".name-list li");
 
 for (user of userList) {
     user.addEventListener("click", function() {
-        console.log(this);
+        this.style.color = "red";
     });
 }
