9901dd2d0ac098c225ffd5fb22636b4859a5ee83
cuauhtemocmartinez <mr.cuauhtemocmartinez@gmail.com>
2021-01-23 16:08:31 -0800

    added appendChild

--- modern_js/app.js
+++ modern_js/app.js
@@ -1,11 +1,13 @@
-const userList = document.querySelectorAll(".name-list li");
+const userList = document.querySelector(".name-list");
 const listInput = document.querySelector(".list-input");
 const addListBtn = document.querySelector(".addListBtn");
 
 addListBtn.addEventListener("click", function(){
     // Create an li out of thin air
-
+    const newLi = document.createElement('LI');
+    const liContent = document.createTextNode(listInput.value);
     // Add the input value inside that new li
-
+    newLi.appendChild(liContent);
     // Atthaching the li to the user list
+    userList.appendChild(newLi);
 });
